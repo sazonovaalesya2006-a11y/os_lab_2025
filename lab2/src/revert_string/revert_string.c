54834070fb42d93b2d4f44637a7bdf967b6414d4
sazonovaalesya2006-a11y <sazonovaalesya2006@gmail.com>
2026-09-06 22:24:44 +0000

    добавлены результаты заданий лабораторной работы 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
 }
-
